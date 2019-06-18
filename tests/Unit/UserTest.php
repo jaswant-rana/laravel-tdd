a20056f57d455ea41ada4b96161b8f96db1337ff
jaswant-rana <jaswant@ucreate.co.in>
2019-06-18 11:03:55 +0530

    setup laravel-tdd and create a single test

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\ParentTestClass;
+use App\Components\CustomQueryBuilder;
+use \Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;    
+
+
+class UserTest extends ParentTestClass
+{
+    use DatabaseTransactions;
+     
+    public function testCreateUser()
+    {
+        $user = factory(User::class)->create();
+        $this->assertInstanceOf(User::class, $user);        
+    }
+    
+    public function testCreateUserWithInvalidEmail()
+    {
+        $user = factory(User::class)->create(['email'=>'jaswant']);
+        $this->assertArrayHasKey('email', $user);
+    }
+}
